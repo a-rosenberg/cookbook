99f9990908c8489972a581f40c0bebfbbc3e5929
a-rosenberg <aaron.matthew.rosenberg@gmail.com>
2017-12-31 23:21:39 -0700

    adding bulk processing code to root dir

---
+++ bulk_changes.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import logging
+import os
+import re
+
+
+def bulk_change(function):
+    for root, dirs, files in os.walk('recipes'):
+        if not root.startswith('./.'):
+            for file in files:
+                if file.endswith('.md'):
+                    path = os.path.join(root, file)
+                    with open(path) as fid:
+                        data = fid.read()
+
+                    output = function(data)
+
+                    with open(path, 'w') as oid:
+                        oid.write(output)
+
+
+def header_h2_to_h1(data):
+    logging.info('header_h2_to_h1 on %s')
+    output = data.replace('h2', 'h1')
+    return output
+
+
+def section_titles_to_h3(data):
+    logging.info('header_h2_to_h1 on %s')
+    output = data.replace('####', '###')
+    return output
+
+
+def list_ingredients(data):
+    recipe = re.findall("<h1.*>(.+)</h1>", data)
+    if recipe:
+        ingredients = re.findall("- (.*)", data)
+        print recipe[0]
+        for ingredient in ingredients:
+            print 'ingredient:', ingredient
+        print
+    return data
+
+
+if __name__ == '__main__':
+    bulk_change(list_ingredients)
