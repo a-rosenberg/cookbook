0b0c328af677913947042a673181faa3c5412196
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2020-01-04 08:52:04 -0500

    update

--- book/move-files.R
+++ book/move-files.R
@@ -1,9 +1,10 @@
-# Move rendered book files into `/doc` directory
-
-dir.create('../docs')
-
-files <- list.files('_book')
-files <- str_c('_book/', files)
-
-file.copy(from = files, 
-          to = '../docs', overwrite = TRUE)
+# # Move rendered book files into `/doc` directory
+# !NOT WORKING
+# 
+# dir.create('../docs')
+# 
+# files <- list.files('_book')
+# files <- str_c('_book/', files)
+# 
+# file.copy(from = files, 
+#           to = '../docs', overwrite = TRUE)
